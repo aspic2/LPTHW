ea75e6c6f1fe17eb0ec51430cc8dcfa1a0e66d57
Thomp <thomp@protonmail.com>
2016-11-14 11:52:18 -0600

    Adding additional lessons

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?", end=" ")
+age = input("Enter your age in years:")
+print("How tall are you?", end=" ")
+height = input("Enter your height in inches:")
+print("How much do you weigh?", end=" ")
+weight = input("Enter your weight in lbs:")
+
+print("So, you are %r years old, %r inches tall, and %r lbs heavy." % (age,
+height, weight))
