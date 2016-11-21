b5cf0222cb5b3245ac68caca225024516f783e94
Thomp <thomp@protonmail.com>
2016-11-20 20:14:28 -0600

    several iterations of ex 33. also, ex34.

---
+++ ex33v3.py
@@ -0,0 +1,29 @@
+
+
+def the_numerator(top):
+    i = 0
+    numbers = []
+    if top > 50:
+        print('number too big. must be < 50.')
+        top = int(input("Please enter a smaller number.\n> "))
+        the_numerator(top)
+    else:
+        for i in range (0,top):
+            print("At the top i is %d" % i)
+            numbers.append(i)
+
+            #i = i + 1
+            print("Numbers now: ", numbers)
+            print("At the bottom i is %d" % i)
+
+
+
+the_numerator(60)
+
+
+
+'''print("The numbers: ")
+
+for num in numbers:
+    print(num, end=' ')
+'''
---
+++ ex34.py
@@ -0,0 +1,25 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#The animal at 1 is the python
+print(animals[1])
+
+#the third animal is the peacock at 2
+print(animals[2])
+
+#the first animal is the bear at 0
+print(animals[0])
+
+#4 the animal at 3 is the kangaroo
+print(animals[3])
+
+#5 the fifth animal is the whale at 4
+print(animals[4])
+
+#6 the animal at 2 is the peacock
+print(animals[2])
+
+#the sixth animal is the platypus at 5
+print(animals.index('platypus'))
+
+#the animal at 4 is the whale
+print(animals.index('whale'))
