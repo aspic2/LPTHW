315979d699ad501787b6cf22c07769db4638609f
Thomp <thomp@protonmail.com>
2016-12-09 15:31:56 -0600

    almost ready. May need to revise questionmaker as a class.

--- quizzical.py
+++ quizzical.py
@@ -1,6 +1,26 @@
 #centralize the quiz functions and just import appropriate dicts
+#revisit commandline arguments. find way to input dictionaries from there
+'''functions/processes for this script:
+    import dict(s)
+    function: make list from dicts
+        return list or Error
+    function: pull random question from list
+        return question or Error
+    function: request input from user
+        return input or Error
+    function: evaluate user input and question
+        increment points or move on
+    function: exit game
+        at end of turns or when user prompts exit
+'''
+
 
 import appropriate dictionaries
+import random
+from sys import exit
+
+
+
 
 def listmaker(source_dict):
     new_list = []
@@ -10,30 +30,49 @@ def listmaker(source_dict):
 
 sessionlist = listmaker(source_dict)
 
-def prompt():
-    pass
+def questionmaker(x):
+    question = random.choice(x)
+    print(question)
+    return question
 
-def finished():
-    pass
+
+def finished(explanation):
+    print(explanation, "\nThanks for playing!")
+    sys.exit(0)
 
 def answerfunction():
-    pass
+    answer = input("> ")
+    return answer
 
 
-def quizzical(turns):
-    turns = input("> ")
+def quizzical(scoring):
+    print("Time to test your knowledge!")
+    turns = input("How many turns would you like?\n> ")
+    try:
+        turns = int(turns)
+    except Exception as e:
+        raise ValueError
+        print("Please enter a whole number.")
+        quizzical(turns)
     points = 0
     gameround = 1
     gameround = turns - gameround
     while gameround <= turns:
         print("-" * 15, "\n\nRound %d" % gameround)
-        question = questionfunction()
-        answer = answerfunction()
-        if answer == question:
-            points += 1
-            print("That is correct! Nice work!")
+        questionmaker()
+        answerfunction()
+        if scoring == True:
+            if answer == question:
+                points += 1
+                print("That is correct! Nice work!")
+            else:
+                print("Sorry, that is incorrect.")
         else:
-            print("Sorry, that is incorrect.")
+            print("Here is the correct answer:")
+            print(source_dict(questionmaker.question))
         gameround += 1
         print("Here's your standing:\n%d turns\n%d points" % (turns, points))
     finished("Nice work!")
+
+if __name__ == '__main__':
+    quizzical(False)
