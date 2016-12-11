cc225b10bd23635f9678254e101f3d1d17599f67
Thomp <thomp@protonmail.com>
2016-12-10 22:29:54 -0600

    error on line 78.

--- quizzical_class.py
+++ quizzical_class.py
@@ -20,40 +20,83 @@ import ex37
 import random
 from sys import exit
 
+session_dict = ex37.full_dict
 
 class QuizInit(object):
 
     def __init__(self, session_dict):
         self.session_dict = session_dict
 
-    def listmaker(self):
-        self.source_dict = self.session_dict
+class ToList(QuizInit):
+    def __init__(self):
+        return None
+
+    def listmaker(self, source_dict):
+        self.source_dict = source_dict
         self.newlist = []
         for key in self.source_dict:
             self.newlist.append(key)
         return self.newlist
 
+class Quizzical(QuizInit):
+    def __init__(self):
+        pass
+
+    def finished(explanation):
+        self.explanation = explanation
+        print(self.explanation, "\nThanks for playing!")
+        exit(0)
+
     def questionmaker(self,somelist):
         self.question = random.choice(self.somelist)
         question = self.question
-        print("Answer/Define this:")
-        print("'", question, "'")
-        return question
+        print("Please define the following value or command:")
+        return self.question
 
     def answerfunction(self):
-        answer = input("> ")
-        return answer
+        self.answer = input("> ")
+        return self.answer
 
+    def quizzical(self, scoring, sessionlist):
+        self.scoring = scoring
+        self.sessionlist = sessionlist
+        print("Time to test your knowledge!")
+        self.turns = input("How many turns would you like?\n> ")
+        if self.turns == "Q" or self.turns == 'q':
+            Quizzical.finished("User quit")
+        else:
+            try:
+                self.turns = int(self.turns)
+                print(self.turns)
+            except Exception as e:
+                print("Please enter a whole number.")
+                quizzical(self, scoring)
+        self.points = 0
+        self.gameround = 1
+        while self.gameround <= self.turns:
+            print("-" * 15, "\n\nRound %d" % self.gameround)
+            Quizzical.questionmaker(self.sessionlist)
+            Quizzical.answerfunction()
+            if self.scoring == True:
+                if answer == question:
+                    self.points += 1
+                    print("That is correct! Nice work!")
+                else:
+                    print("Sorry, that is incorrect.")
+            else:
+                print("Here is the correct answer:")
+                print(QuizInit.session_dict[questionmaker.question])
+            self.gameround += 1
+            print("Here's your standing:\n%d turns\n%d points" % (self.turns, self.points))
+        Quizzical.finished("\n\nFinished all rounds!")
 
-def finished(explanation):
-    print(explanation, "\nThanks for playing!")
-    sys.exit(0)
+quizinit = QuizInit(session_dict)
+tolist = ToList()
+listmaker = tolist.listmaker(session_dict)
+quizzical = Quizzical()
+print(listmaker)
+session_quiz = quizzical.quizzical(False, listmaker)
 
-
-keywordsquiz = QuizInit(ex37.keywords_dict)
-sessionlist = keywordsquiz.listmaker()
-new_q = keywordsquiz.questionmaker(sessionlist)
-print(new_q)
 
 
 '''
