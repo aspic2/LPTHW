210f6e117805469521df79cdc946e4fcbb3716a9
Thomp <thomp@protonmail.com>
2016-12-15 14:06:22 -0600

    IT WORKS!

--- ex43.py
+++ ex43.py
@@ -9,11 +9,6 @@ Aliens have invaded a space ship and our hero has to go through a
  print its own description when the player enters it and then tell the
  engine what room to run next out of the map.
  '''
-'''
-*Player initializes. *Player moves to first *Scene. Player completes Scene.task
-Player moves to next Scene
-
-'''
 
 '''
 Scenes
@@ -70,15 +65,15 @@ class Engine(object):
         self.scene_map = scene_map
 
     def play(self):
-        current_scene = self.scene_map.opening_scene()
-        last_scene = self.scene_map.next_scene('youwin')
+        print("-" * 20)
+        print("GOTHONS FROM PLANET PERCAL #25")
+        start = self.scene_map.scenes['cc']
+        final = self.scene_map.scenes['youwin']
 
-        while current_scene != last_scene:
-            next_scene_name = current_scene.enter()
-            current_scene = self.scene_map.next_scene(next_scene_name)
+        while start != final:
+            start = self.scene_map.scenes[start.enter()]
 
-        # be sure to print out the last scene
-        current_scene
+        self.scene_map.scenes[final.enter()]
 
 class Death(Scene):
 
@@ -92,26 +87,34 @@ class Death(Scene):
 
 class CentralCorridor(Scene):
 
-    description = "This is the Central Corridor.\nYou find yourself in an unfamiliar dark hallway, and your head hurts.\nYou look around to get your bearings. You realize you are on an alien ship.\nJudging by the view out the window, it is headed for planet Percal #25.\n\nYou need to get out of here, and quick. There's no escape once you reach the planet.\nThese ships typically have ecape pods, so you'll need to find one to get away.\nAdditionally, you will need to clear the ship of Gothons so they don't chase you down."
+    description = ("This is the Central Corridor.",
+    "You find yourself in an unfamiliar dark hallway, and your head hurts.",
+    "You look around to get your bearings. You realize you are on an alien ship.",
+    "Judging by the view out the window, it is headed for planet Percal #25, home of the Gothons.",
+    "You need to get out of here, and quick. There's no escape once you reach the planet.",
+    "These ships typically have ecape pods, so you'll need to find one to get away.",
+    "Additionally, you will need to clear the ship of Gothons so they don't chase you down.\n\n")
 
     def enter():
-        print(CentralCorridor.description)
+        for line in CentralCorridor.description:
+            print(line)
         step1 = input("Do you head forward through the hall or back?\n> ")
         if step1 == 'forward':
             print("You run into your first Gothon")
             step2 = input("You can fight, run, or outdance it. Which do you choose?\n> ")
             if 'outdance' in step2:
                 print("That Gothon was no match for your moves.")
-                print("You proceed to the Light Weapons Armory.")
+                print("You proceed to the Laser Weapons Armory.\n\n\n")
                 return 'lwa'
             else:
                 print("That was a bad idea.")
-                print("Gothons are fast and very good at fighting.")
+                print("This Gothon was all state in high school at track and boxing.")
+                print("He destroys you, broh.\n\n")
                 return 'death'
         else:
             print("You stall and stall. Eventually you land on Percal #25.")
             print("The Gothons disembark the ship and find you cowering in a corner.")
-            print("They rip you to shreds.")
+            print("They rip you to shreds.\n\n")
             return 'death'
 
 
@@ -126,30 +129,36 @@ class Player(object):
 
 class LaserWeaponArmory(Scene):
 
-    description = "This is the Laser Weapon Armory. To your surprise, there are more than laser weapons in here. You find a pretty cool bomb that you think can take out the whole ship. You hatch a plan to time it to detonate after some minutes, which should give you enough time to escape."
+    description = "This is the Laser Weapon Armory.", \
+    "To your surprise, there are more than laser weapons in here.",\
+    "You find a pretty cool bomb that you think can take out the whole ship.",\
+    "You hatch a plan to time it to detonate after some minutes, which should give you enough time to escape."
 
     def enter():
-        print(LaserWeaponArmory.description)
-        print("The bomb has a keypad lock on it. Can you guess the one digit code keeping it locked? (Gothons are not very good at weapons security.)")
+        for line in LaserWeaponArmory.description:
+            print(line)
+        print("The bomb has a keypad lock on it.",\
+        "Can you guess the one digit code keeping it locked?",\
+        "(Gothons are not very good at weapons security.)")
         correct_code = random.randint(0,10)
         print(correct_code)
         tries = 0
-        while tries < 10:
+        while tries < 5:
             guess = input("Enter a number\n> ")
             try:
                 guess = int(guess)
             except ValueError:
-                print("Don't be a funny guy. Enter a regular keypad number.")
-                LaserWeaponArmory.enter()
+                print("Nice going, funny guy. You broke the keypad.\n\n")
+                return 'death'
             if guess != correct_code:
                 print("\a\a\a\aBZZZZDT!")
                 tries += 1
-                print("Try again. %d more chances." % 10 - tries)
+                print("Try again. %d more chances." % (5 - tries))
             else:
                 print("Nice. You have 10 minutes on the clock.")
-                print("Take that bomb to the Bridge and blow this ship to smithereens!")
+                print("Take that bomb to the Bridge and blow this ship to smithereens!\n\n\n")
                 return 'bridge'
-        print("Out of chances. The bomb has engaged with 5 seconds on the clock. Realizing you can't outrun it, you stand there with a sad look on your face and await your fate.")
+        print("Out of chances. The bomb has engaged with 5 seconds on the clock. Realizing you can't outrun it, you stand there with a sad look on your face and await your fate.\n\n")
         return 'death'
 
 
@@ -161,11 +170,14 @@ class TheBridge(Scene):
     def enter():
         print(TheBridge.description)
         step1 = input("Do you give up or try to outsmart them?\n> ")
-        if step1 == 'outsmart':
-            print("'Smart' thinking! You point your gun at the bomb. They know what that means and they slowly back away from you. Two more come by, see what's going on and sneak away as though they were never here. You have set the bomb and now need to get out of here.")
+        if 'outsmart' in step1:
+            print("'Smart' thinking! You point your gun at the bomb.", \
+            "They know what that means and they slowly back away from you.", \
+            "Two more come by, see what's going on, and sneak away as though they were never here.", \
+            "You have set the bomb and now need to get out of here.\n\n\n")
             return 'ep'
         else:
-            print("You sniveling worm. After all this work, you give up?")
+            print("You sniveling worm. After all this work, you give up? Death ensues.\n\n")
             return 'death'
 
 class EscapePod(Scene):
@@ -178,7 +190,7 @@ class EscapePod(Scene):
         if step1 == '1' or step1 == '2' or step1 == '3':
             return 'youwin'
         else:
-            print("You really should listen to me more...")
+            print("You really should listen to me more...\n\n\n")
             return 'death'
 
 
@@ -187,19 +199,19 @@ class YouWin(Scene):
     description = "Nice work! You picked a good pod and you blast out into space. A few seconds go by and you hear a shockwave. The Gothon shop ignited like a sun. You sit back, relax, and catch some Z's as you head home."
 
     def enter():
-        print(Death.description)
+        print(YouWin.description)
         return exit(0)
 
 
 class Map(object):
 
     scenes = {
-    'cc': CentralCorridor.enter(),
-    'lwa': LaserWeaponArmory.enter(),
-    'bridge': TheBridge.enter(),
-    'ep': EscapePod.enter(),
-    'death': Death.enter(),
-    'youwin': YouWin.enter()
+    'cc': CentralCorridor,
+    'ep': EscapePod,
+    'bridge': TheBridge,
+    'lwa': LaserWeaponArmory,
+    'death': Death,
+    'youwin': YouWin
     }
 
     def __init__(self, start_scene):
@@ -213,6 +225,11 @@ class Map(object):
 
 
 #my version
+
 start_map = Map('cc')
 new_game = Engine(start_map)
 new_game.play()
+
+
+#test_scene = CentralCorridor.enter()
+#print(test_scene)
