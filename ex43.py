b857b8b10dc250352a7bc16796b6f0eb63c4ef55
Thomp <thomp@protonmail.com>
2016-12-19 09:12:12 -0600

    updated block comments

--- ex43.py
+++ ex43.py
@@ -24,27 +24,10 @@ class Scene(object):
         print(self.description)
 
 
-class Engine(object):
-
-    def __init__(self, scene_map):
-        self.scene_map = scene_map
-
-    def play(self):
-        print("-" * 50)
-        print("GOTHONS FROM PLANET PERCAL #25")
-        start = self.scene_map.scenes['cc']
-        final = self.scene_map.scenes['youwin']
-
-        while start != final:
-            start = self.scene_map.scenes[start.enter()]
-
-        self.scene_map.scenes[final.enter()]
-
-
 class Death(Scene):
 
     description = "Death is always right around the corner. \
-    \nLooks like it finally caught up with you.\n[You died]"
+    \nLooks like it finally caught up with you.\n\n[You died]"
 
     def enter():
         print(Death.description)
@@ -52,10 +35,12 @@ class Death(Scene):
 
 
 class Quit(Scene):
+
     description = "User quit.\nchicken..."
     def enter():
         print(Quit.description)
         return exit(0)
+
 
 class CentralCorridor(Scene):
 
@@ -71,12 +56,12 @@ class CentralCorridor(Scene):
         for line in CentralCorridor.description:
             print(line)
         step1 = input("Do you head forward through the hall or back away into a corner?\n> ")
-        if step1 == 'q':
+        if step1 == 'q' or step1 == 'quit':
             return 'quit'
         elif 'forward' in step1:
             print("As you proceed forward, you run into your first Gothon.")
             step2 = input("You can fight, run, or outdance it. Which do you choose?\n> ")
-            if step2 == 'q':
+            if step2 == 'q' or step2 =='quit':
                 return 'quit'
             elif 'outdance' in step2:
                 print("That Gothon was no match for your moves.")
@@ -104,15 +89,16 @@ class LaserWeaponArmory(Scene):
     def enter():
         for line in LaserWeaponArmory.description:
             print(line)
-        print("The bomb has a keypad lock on it.",\
+        print("\nThe bomb has a keypad lock on it.",\
         "Can you guess the one digit code keeping it locked?",\
         "(Gothons are not very good at weapons security.)")
+        # why not make correct_code a string and get rid of try/except block?
         correct_code = random.randint(0,10)
-        print(correct_code)
+        print("Hint: it might be [%d]" % correct_code)
         tries = 0
         while tries < 5:
             guess = input("Enter a number\n> ")
-            if guess == 'q':
+            if guess == 'q' or guess == 'quit':
                 return 'quit'
             try:
                 guess = int(guess)
@@ -127,18 +113,22 @@ class LaserWeaponArmory(Scene):
                 print("Nice. You have 10 minutes on the clock.")
                 print("Take that bomb to the Bridge and blow this ship to smithereens!\n\n\n")
                 return 'bridge'
-        print("Out of chances. The bomb has engaged with 5 seconds on the clock. Realizing you can't outrun it, you stand there with a sad look on your face and await your fate.\n\n")
+        print("Out of chances. The bomb has engaged with 5 seconds on the clock.")
+        print("Realizing you can't outrun it, you stand there with a sad look on your face and await your fate.\n\n")
         return 'death'
 
 
 class TheBridge(Scene):
 
-    description = "This is The Bridge. There are several Gothons guarding it, too many to outdance, even for you. You try to sneak up on them, but they hear you and point their guns at you"
+    description = "This is The Bridge.",\
+    "There are several Gothons guarding it, too many to outdance, even for you.",\
+    "You try to sneak up on them, but they hear you and point their guns at you."
 
     def enter():
-        print(TheBridge.description)
+        for line in TheBridge.description:
+            print(line)
         step1 = input("Do you give up or try to outsmart them?\n> ")
-        if step1 == 'q':
+        if step1 == 'q' or step1 == 'quit':
             return 'quit'
         elif 'outsmart' in step1:
             print("'Smart' thinking! You point your gun at the bomb.", \
@@ -158,8 +148,8 @@ class EscapePod(Scene):
     def enter():
         print(EscapePod.description)
         step1 = input("There are three pods here. Do you want pod 1, 2, or 3?\n> ")
-        #correct_pod = random.randint(1,3)
-        if step1 == 'q':
+        #removed code: correct_pod = random.randint(1,3)
+        if step1 == 'q' or step1 == 'quit':
             return 'quit'
         elif step1 == '1' or step1 == '2' or step1 == '3':
             return 'youwin'
@@ -170,10 +160,11 @@ class EscapePod(Scene):
 
 class YouWin(Scene):
 
-    description = "Nice work! You picked a good pod and you blast out into space. A few seconds go by and you feel a shockwave. The Gothon ship ignites like a sun. You sit back, relax, and catch some Z's as you head home."
+    description = "Nice work! You picked a good pod and you blast out into space.", "A few seconds go by and you feel a shockwave.", "The Gothon ship ignites like a sun.", "You sit back, relax, and catch some Z's as you head home.", "\n[end of game]"
 
     def enter():
-        print(YouWin.description)
+        for line in YouWin.description:
+            print(line)
         return exit(0)
 
 
@@ -197,6 +188,24 @@ class Map(object):
 
     def opening_scene(self):
         Map.scenes['cc']
+
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        print("-" * 50)
+        print("GOTHONS FROM PLANET PERCAL #25")
+        start = self.scene_map.scenes['cc']
+        final = self.scene_map.scenes['youwin']
+
+        while start != final:
+            start = self.scene_map.scenes[start.enter()]
+
+        self.scene_map.scenes[final.enter()]
+
 
 
 start_map = Map('cc')
