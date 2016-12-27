d740d62c135db09dd86969cbdfac9fc23dd8f7b3
Thomp <thomp@protonmail.com>
2016-12-26 19:44:14 -0600

    new board logic. Now a single list, so spaces are marked by a single index (e.g. '2')

--- ttt.py
+++ ttt.py
@@ -1,7 +1,5 @@
 #Command line tic tac toe
 '''
-Problem: create an entertaining game that will push my knowledge of programming
-
 Analysis and Design:
   App must be able to:
       1) Render board (3x3) on screen with placed markers
@@ -52,33 +50,46 @@ Scoring:
 
     1 pt per mark on screen
     3 pts for three in row
+
+Things currently not working:
+show available spaces
+
+
 '''
+
 
 class Board(object):
     def __init__(self, rows):
         self.gameboard = []
-        self.rows = rows
-        for row in range(self.rows):
-            self.gameboard.append(["_"] * self.rows)
+        self.spaces = rows ** 2
+        self.possiblemoves = []
+        for row in range(self.spaces):
+            self.gameboard.append("_")
 
-    def getSpace(self, xvalue, yvalue):
-        print(self.gameboard[xvalue][yvalue])
+    def getSpace(self, space):
+        print(self.gameboard[space])
 
     #does not work
     def showOpenSpaces(self):
         self.possiblemoves = []
-        for x in self.gameboard:
-            if x == "_":
+        for x in range(self.spaces):
+            if self.gameboard[x] == "_":
                 self.possiblemoves.append(x)
         return self.possiblemoves
 
-    def updateBoard(self, xcoord, ycoord):
-        self.gameboard[xcoord][ycoord] = "M"
+    def updateBoard(self, square):
+        self.gameboard[square] = "M"
         return self.gameboard
 
     def print_board(self):
-        for row in self.gameboard:
-            print("|".join(row))
+        print("\n")
+        print(self.gameboard[0], "|", self.gameboard[1], "|", self.gameboard[2])
+        print("--+---+--")
+        print(self.gameboard[3], "|", self.gameboard[4], "|", self.gameboard[5])
+        print("--+---+--")
+        print(self.gameboard[6], "|", self.gameboard[7], "|", self.gameboard[8])
+        print("--+---+--")
+        print("\n")
         #return self.gameboard
 
 
@@ -88,7 +99,7 @@ class Player(object):
 
 
     def makeMove(self):
-        #move = input("Place your marker:\n[xcoordinate],[ycoord]")
+        #move = input("Place your marker:\n[xcoordinate], [ycoord]")
         row = input("Guess the row:")
         try:
             row = int(row)
@@ -106,6 +117,7 @@ class Player(object):
 class AI(Player):
 
     def randomMove():
+        pass
 
 
 class Gameplay(object):
@@ -114,17 +126,14 @@ class Gameplay(object):
 
 
 
+b = Board(3)
+b.print_board()
+b.updateBoard(1)
+b.print_board()
+b.getSpace(1)
+b.getSpace(0)
 
-tictactoe = Board(3)
-#tictactoe.print_board()
-
-print(tictactoe.gameboard)
-print(type(tictactoe.gameboard))
-me = Player()
-tictactoe.updateBoard(1,1)
-tictactoe.print_board()
-
-tictactoe.getSpace(1,1)
+print(b.showOpenSpaces())
 
 
 
