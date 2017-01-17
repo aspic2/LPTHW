58278a5d66f95bf542279f85cef241453bbba4da
Thomp <thomp@protonmail.com>
2017-01-16 21:50:55 -0600

    revised project skeleton

---
+++ ex48/ex48/lexicon.py
@@ -0,0 +1,9 @@
+#scanner project goes here
+
+def scan(userinput):
+    #create list or dictionary of parts of speech, maybe expected
+    #words for input
+    #split userinput into separate words and punctuation
+    #match userinput with parts of parts of speech
+    #return tuples containing matched parts of speech
+    
