28129002a0fab6e8530d30c7331ac88a4005c7b0
Thomp <thomp@protonmail.com>
2017-01-24 20:09:18 -0600

    slowly figuring out parsers

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -1,9 +1,21 @@
 #scanner project goes here
 
 def scan(userinput):
+    vocab = {'north': 'direction'}
+    words = userinput.lower().split()
     #create list or dictionary of parts of speech, maybe expected
     #words for input
     #split userinput into separate words and punctuation
-    #match userinput with parts of parts of speech
-    #return tuples containing matched parts of speech
-    
+    #make list of tuples containing userinput words and their types
+    #create sentence by using putting tuple list words in order by type 
+    print(words)
+
+    #for word in words:
+    #    if word in vocab:
+
+
+
+
+userinput = "Ample parking day or night. People shouting \"Howdy, neighbor!\""
+
+scan(userinput)
