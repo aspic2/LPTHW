551030a213cc23d536c3005c136ec723b8bf961b
Thomp <thomp@protonmail.com>
2017-02-01 06:16:44 -0600

    added all functions. need to build test and check ParserError class for problems.

--- ex49/ex49/parser.py
+++ ex49/ex49/parser.py
@@ -28,3 +28,46 @@ def match(word_list, expecting):
             return None
     else:
         return None
+
+def skip(word_list, word_type):
+    while peek(word_list) == word_type:
+        match(word_list, word_type)
+
+def parse_verb(word_list):
+    skip(word_list, 'stop')
+
+    if peek(word_list) == 'verb':
+        return match(word_list, 'verb')
+    else:
+        raise ParserError("Expected a verb next.")
+
+def parse_object(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'direction':
+        return match(word_list, 'direction')
+    else:
+        raise ParserError("Expected a noun or direction next.")
+
+def parse_subject(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'verb':
+        return('noun', 'player')
+    else:
+        raise ParserError("Expected a verb next.")
+
+def parse_sentence(word_list):
+    subj = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
+
+    return Sentence(subj, verb, obj)
+
+    
