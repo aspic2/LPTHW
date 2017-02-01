a18e851fc01940c45f4dc6f0e0c8d8bac703ee18
Thomp <thomp@protonmail.com>
2017-02-01 16:10:58 -0600

    setup for tests. not functioning yet, but shell is there.

--- ex49/tests/parser_tests.py
+++ ex49/tests/parser_tests.py
@@ -1,5 +1,33 @@
-from nose.tools import *
-from ex49 import parser
+#test functionality of parser
 
-def test_construction():
+from nose.tools import *
+from parser import *
+from ex49 import lexicon
+
+class TestSentence(object):
+    def make_wordlist(sentence):
+        return scan(sentence)
+
+testsentence = TestSentence.make_wordlist("Run north and kill the bear")
+
+def test_peek():
+    parser.peek.word_list = testsentence
+    print(parser.peek(word_list))
+
+def test_match():
+    pass
+
+def test_skip():
+    pass
+
+def test_parse_verb():
+    pass
+
+def test_parse_object():
+    pass
+
+def test_parse_subject():
+    pass
+
+def test_parse_sentence():
     pass
